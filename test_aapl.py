7c31d227675c62eaa5a565ad8ddb36e69ac2d314
sushmaj90 <sushmahampapur@gmail.com>
2015-10-08 13:03:01 -0500

    Create test_aapl.py

---
+++ test_aapl.py
@@ -0,0 +1,13 @@
+#Refer to aapl.json and aapl.dat for files required to run the program
+import json
+import yahoo_options_data
+computedJson = yahoo_options_data.contractAsJson("aapl.dat")
+expectedJson = open("aapl.json").read()
+expectedJson_change = open("aapl_change.json").read()
+if json.loads(computedJson) != json.loads(expectedJson) and json.loads(computedJson) != json.loads(expectedJson_change):
+    print "Test failed!"
+    print "Expected output:", expectedJson
+    print "Your output:", computedJson
+    assert False
+else:
+    print "Test passed"
